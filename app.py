a7b2dd0ed0a87fc42da791f18c4b818fe1ec48e3
BornToBeLoved <sungwon1685@gmail.com>
2022-03-23 22:45:14 +0900

    [TEST] http request test #1

--- app.py
+++ app.py
@@ -2,15 +2,6 @@ import requests
 import json
 import account.BE_http as BE
 
-# google = requests.get("http://google.com")
-# print(google)
-
-
-# print("http://"+BE.ip+":"+BE.port+"/textmining")
-
-# r = requests.get("http://"+BE.ip+":"+"15044/test").text #request 객체를 문자로 변환.
-# print(r) 
-
 r = requests.post("http://"+BE.ip+":"+BE.port+"/preprocessing", json = {
     "userEmail": "21800520@handong.edu",
     "keyword": "북한",
