0a409fb18b56947fe2f16a3c1577d34baad4d004
BornToBeLoved <sungwon1685@gmail.com>
2022-03-27 18:35:46 +0900

    [FEAT] initial flask server #3

--- app.py
+++ app.py
@@ -1,28 +1,48 @@
+# 기존 flask에서 사용한 모듈 전체 적용
+from flask import Flask, jsonify, request, Response, render_template, copy_current_request_context, current_app, abort
+
 import requests
 import json
-import account.BE_http as BE
+import account.BE_flask as BE
 
-r = requests.post("https://"+BE.ip+":"+BE.port+"/preprocessing",verify=False, json = {
-    "userEmail": "21800520@handong.edu",
-    "keyword": "북한",
-    "savedDate": "2021-09-07T07:01:07.137Z",
-    "synonym": False,
-    "stopword": False,
-    "compound": False,
-    "wordclass": "010"
-})
-
-print(r.text)
-
-r = requests.post("https://"+BE.ip+":"+BE.port+"/textmining",verify=False, json ={
-    "userEmail": "21800520@handong.edu",
-    "keyword": "북한",
-    "savedDate": "2021-09-07T07:01:07.137Z",
-    "option1": "100",
-    "option2": None,
-    "option3": None,
-    "analysisName": "count"
-    }).text
+# flask 객체
+app = Flask(__name__) # 정적 파일과 템플릿을 찾는데 쓰인다고 한다. 무슨소리일까..
 
 
-print(r)
+
+
+@app.route('/preprocessing', methods=['GET', 'POST'])
+def test():
+    r = requests.post("https://"+BE.ip+":"+BE.port+"/preprocessing",verify=False, json = {
+        "userEmail": "21800520@handong.edu",
+        "keyword": "북한",
+        "savedDate": "2021-09-07T07:01:07.137Z",
+        "synonym": False,
+        "stopword": False,
+        "compound": False,
+        "wordclass": "010"
+    })
+
+    print(r.text)
+
+    r = requests.post("https://"+BE.ip+":"+BE.port+"/textmining",verify=False, json ={
+        "userEmail": "21800520@handong.edu",
+        "keyword": "북한",
+        "savedDate": "2021-09-07T07:01:07.137Z",
+        "option1": "100",
+        "option2": None,
+        "option3": None,
+        "analysisName": "count"
+        }).text
+
+    print(r)
+    return r
+
+import account.FE_flask as FERS
+import account.kubic_sslFile as kubic_ssl
+
+if __name__ == "__main__":
+
+    context=(kubic_ssl.crt,kubic_ssl.key)
+    app.run(host=FERS.ip, port=FERS.port, ssl_context=context, debug=True)
+
