7b5f3c95ffe06dac8142346ae671bfff3d883fa6
BornToBeLoved <sungwon1685@gmail.com>
2022-07-27 21:22:07 +0900

    [TEST] Service test complete #9

--- app.py
+++ app.py
@@ -47,37 +47,37 @@ def tfidfTable():
     return r
 
 
-@app.route('/test', methods=['GET', 'POST'])
-def test():
-    if request.method == 'GET':
-        r = requests.post("https://"+BE.ip+":"+BE.port+"/preprocessing",verify=False, json = {
-            "userEmail": "21800520@handong.edu",
-            "keyword": "북한",
-            "savedDate": "2021-09-07T07:01:07.137Z",
-            "synonym": False,
-            "stopword": False,
-            "compound": False,
-            "wordclass": "010"
-        })
+# @app.route('/test', methods=['GET', 'POST'])
+# def test():
+#     if request.method == 'GET':
+#         r = requests.post("https://"+BE.ip+":"+BE.port+"/preprocessing",verify=False, json = {
+#             "userEmail": "21800520@handong.edu",
+#             "keyword": "북한",
+#             "savedDate": "2021-09-07T07:01:07.137Z",
+#             "synonym": False,
+#             "stopword": False,
+#             "compound": False,
+#             "wordclass": "010"
+#         })
 
-        print(r.text)
+#         print(r.text)
 
-        r = requests.post("https://"+BE.ip+":"+BE.port+"/textmining",verify=False, json ={
-            "userEmail": "21800520@handong.edu",
-            "keyword": "북한",
-            "savedDate": "2021-09-07T07:01:07.137Z",
-            "option1": "100",
-            "option2": None,
-            "option3": None,
-            "analysisName": "count"
-            }).text
+#         r = requests.post("https://"+BE.ip+":"+BE.port+"/textmining",verify=False, json ={
+#             "userEmail": "21800520@handong.edu",
+#             "keyword": "북한",
+#             "savedDate": "2021-09-07T07:01:07.137Z",
+#             "option1": "100",
+#             "option2": None,
+#             "option3": None,
+#             "analysisName": "count"
+#             }).text
 
-        print(r)
-        return r
-    elif request.method == 'POST':
-        data = request.json
+#         print(r)
+#         return r
+#     elif request.method == 'POST':
+#         data = request.json
 
-        return "사용자"+data["userEmail"]+"이(가) 확인되었습니다."
+#         return "사용자"+data["userEmail"]+"이(가) 확인되었습니다."
 
 import account.FE_flask as FERS
 import account.kubic_sslFile as kubic_ssl
